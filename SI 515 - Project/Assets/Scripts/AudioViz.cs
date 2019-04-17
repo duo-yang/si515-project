8e66210d0713cb6765a61b6754cdf6507289b714
Duo YANG <yangduots@163.com>
2019-04-17 02:15:32 -0400

    Add audio

---
+++ SI 515 - Project/Assets/Scripts/AudioViz.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioViz : MonoBehaviour {
+  private AudioSource _audioSource;
+  public float[] samples = new float[512];
+
+	// Use this for initialization
+	void Start () {
+    _audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    getSpectrumfromAudioSource();
+	}
+
+  public void getSpectrumfromAudioSource (AudioSource source) {
+    if (source != null) source.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+  }
+
+  public void getSpectrumfromAudioSource () {
+    getSpectrumfromAudioSource(_audioSource);
+  }
+
+}
